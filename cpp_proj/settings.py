f59afda11724a835fe59ecb343633e5189d64ba4
ashwin <x23196505@student.ncirl.ie>
2024-03-05 19:15:32 +0000

    added login functionality

--- cpp_proj/settings.py
+++ cpp_proj/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# LOGIN_REDIRECT_URL = '/tables'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
---
+++ users/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from .forms import UserSignUpForm,LoginForm
+
+def sign_up(request):
+    if request.method == "POST":
+        form = UserSignUpForm(request.POST)
+        if form.is_valid():
+            user=form.save(commit=False)
+            user.is_staff=True
+            user.save()
+            un = form.cleaned_data.get('username')
+            messages.success(request, 'Account created for {}.'.format(un))
+            return redirect('sign_in')
+    elif request.method == "GET":
+        form = UserSignUpForm()
+    return render(request, 'users/signup.html', {'form': form})
+    
+def signin(request):
+    form = LoginForm(request.POST or None)
+    msg = None
+    if request.method == 'POST':
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None and user.is_staff:
+                login(request, user)
+                return redirect('tables')
+            elif user is not None and user.is_staff==False:
+                login(request, user)
+                return redirect('index')
+            else:
+                msg= 'invalid credentials'
+        else:
+            msg = 'error validating form'
+    return render(request, 'users/signin.html', {'form': form, 'msg': msg})
